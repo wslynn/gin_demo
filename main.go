f424beaebc9975ef647bb265996f51a4e047ff47
wangshuang <ws156858@163.com>
2021-10-12 00:03:46 +0800

    feat: 一个简单的gin程序

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	r := gin.Default() // 返回默认的路由引擎
+	r.GET("/hello", sayHello)
+	r.GET("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "get",
+		})
+	})
+	r.POST("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "post",
+		})
+	})
+
+	r.Run(":9090")
+}
+
+func sayHello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello golang!",
+	})
 }
