bee3d0992b9d0287f5974cd22f739757a295649a
wangshuang <ws156858@163.com>
2021-10-16 11:25:00 +0800

    feat: gin模板渲染

--- main.go
+++ main.go
@@ -1,41 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/hello", sayHello)
-	err := http.ListenAndServe(":9090", nil)
-	if err != nil {
-		fmt.Printf("http serve failed, err:%v", err)
-		return
-	}
-}
-
-func sayHello(w http.ResponseWriter, r *http.Request) {
-	// 读取模板文件
-	t, err := template.ParseFiles("./hello.tmpl")
-	if err != nil {
-		fmt.Println("ParseFiles failed, err:%v", err)
-		return
-	}
-	// 渲染模板, .表示当前对象
-	err = t.Execute(w, "小王")
-	if err != nil {
-		fmt.Println("Execute failed, err:%v", err)
-		return
-	}
-}
-
-func tmplDemo(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("./t.tmpl", "./ul.tmpl")
-	if err != nil {
-		fmt.Println("create template failed, err:", err)
-		return
-	}
-	user := UserInfo{Name: "小王子", Gender: "男", Age: 18}
-	tmpl.Execute(w, user)
+	r := gin.Default()
+	r.LoadHTMLFiles("templates/index.html")
+	r.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", gin.H{
+			"title": "wangshuang.com",
+		})
+	})
+	r.Run(":9090")
 }
