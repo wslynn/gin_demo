a0cb9f93cd10e7c5e743257d1e2bfab91cad2e7c
wangshuang <ws156858@163.com>
2021-10-18 23:39:31 +0800

    feat: gin静态文件路径映射, 自定义js函数

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"html/template"
 	"net/http"
 )
 
 func main() {
 	r := gin.Default()
+	// 设置静态文件路径
+	r.Static("/xxx", "./static")
+	// 设置自定义的js函数
+	r.SetFuncMap(template.FuncMap{
+		"safe": func(str string) template.HTML {
+			return template.HTML(str)
+		},
+	})
 	r.LoadHTMLFiles("templates/index.html")
+
 	r.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.html", gin.H{
-			"title": "wangshuang.com",
-		})
+		c.HTML(http.StatusOK, "index.html", "<a href='https://www.baidu.com'>百度</a>")
 	})
 	r.Run(":9090")
 }
