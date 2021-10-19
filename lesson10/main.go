ac8fde34663c12d69d714f39ba8babd864d55cec
wangshuang <ws156858@163.com>
2021-10-19 23:15:45 +0800

    feat: gin返回json的几种方式

--- lesson10/main.go
+++ lesson10/main.go
@@ -9,11 +9,24 @@ func main() {
 	r := gin.Default()
 	r.GET("/index", func(c *gin.Context) {
 		// 方法1: 使用map
-		data := map[string]interface{}{
-			"name":    "小王子",
-			"message": "hello world",
-			"age":     18,
+		//data := map[string]interface{}{
+		//	"name":    "小王子",
+		//	"message": "hello world",
+		//	"age":     18,
+		//}
+		// 方法2: 使用gin.H
+		//data := gin.H{
+		//	"name":    "小王子",
+		//	"message": "hello world",
+		//	"age":     18,
+		//}
+		// 方法3: 使用结构体, 变量名必须大写才能导出, 利用tag变为小写
+		type msg struct {
+			Name    string `json:"name"`
+			Message string
+			Age     int
 		}
+		data := msg{"小王子", "hello world", 18}
 		c.JSON(http.StatusOK, data)
 	})
 	r.Run(":9090")
